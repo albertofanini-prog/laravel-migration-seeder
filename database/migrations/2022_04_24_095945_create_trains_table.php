79ec8fd33d85a2283a0346cd381c7b71c8c48ac4
albertofanini-prog <albertofanini@outlook.com>
2022-04-24 15:35:00 +0200

    Step 1 - Tabella trains con migration

---
+++ database/migrations/2022_04_24_095945_create_trains_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTrainsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('trains', function (Blueprint $table) {
+            $table->id();
+            $table->string('brand', 25);
+            $table->string('arrive_station', 50);
+            $table->string('departure_station',50);
+            $table->timeTz('arrive_hour');
+            $table->timeTz('departure_hour');
+            $table->smallInteger('train_code');
+            $table->tinyInteger('number_of_carriage');
+            $table->boolean('in_time')->default(1);
+            $table->boolean('deleted')->default(1);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('trains');
+    }
+}
